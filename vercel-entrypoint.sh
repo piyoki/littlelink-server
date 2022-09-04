c8810e84ef6c27d76709380176e8e4dd044d182b
yqlbu <yqlbu@bu.edu>
2022-09-04 23:41:12 +0800

    removed test script

--- vercel-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-originalfile="env.js"
-tmpfile=$(mktemp)
-cp --attributes-only --preserve $originalfile $tmpfile
-cat $originalfile | tee $tmpfile && mv $tmpfile $originalfile
-exec "$@"
